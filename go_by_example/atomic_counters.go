119fdbfded522b623c3c9bdbe98663452a40c1bb
anbreakdy <anbreakdy@outlook.com>
2022-09-29 09:48:04 +0800

    bofore stateful_goroutines

--- go_by_example/atomic_counters.go
+++ go_by_example/atomic_counters.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	var ops uint64
+	var wg sync.WaitGroup
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for c := 0; c < 10000; c++ {
+				atomic.AddUint64(&ops, 1)
+				//ops += 1
+			}
+		}()
+	}
+	wg.Wait()
+	fmt.Println("ops:", ops)
+}

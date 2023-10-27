3bf1629998ababa1c41744d774033ada6ee8b540
anbreakdy <anbreakdy@outlook.com>
2023-10-27 10:04:45 +0800

    add comment

--- go_web_programming/first_webapp.go
+++ go_web_programming/first_webapp.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// 可以直接返回HTML浏览器就可以展示
 func handler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "<html><body><h1>Hello, World! %s </h1></body></html>", request.URL.Path[1:])
 }

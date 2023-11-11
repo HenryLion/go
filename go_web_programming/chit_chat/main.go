2e7a5caaa5aaa45680b72b6c7573ab873a5078a0
anbreakdy <anbreakdy@outlook.com>
2023-11-12 01:12:31 +0800

    add go module

--- go_web_programming/chit_chat/main.go
+++ go_web_programming/chit_chat/main.go
@@ -1,49 +1,24 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 )
-
-func index(w http.ResponseWriter, r *http.Request) {
-	threads, err := data.Threads()
-	if err == nil {
-		_, err := session(w, r)
-		public_tmpl_files := []string{"templates/layout.html",
-			"templates/public.navbar.html",
-			"templates/index.html"}
-		private_tmpl_files := []string{"templates/layout.html",
-			"templates/private.navbar.html",
-			"templates/index.html"}
-		var templates *template.Template
-		if err != nil {
-			templates = template.Must(template.ParseFiles(private_tmpl_files...))
-		} else {
-			templates = template.Must(template.ParseFiles(public_tmpl_files...))
-		}
-		templates.ExecuteTemplate(w, "layout", threads)
-	}
-
-	if err == nil {
-		templates.ExecuteTemplate(w, "layout", threads)
-	}
-}
 
 func main() {
 	mux := http.NewServeMux()
 	files := http.FileServer(http.Dir("/public"))
 	mux.Handle("/static", http.StripPrefix("/static", files))
 	mux.HandleFunc("/", index)
-	mux.HandleFunc("/err", err)
-	mux.HandleFunc("/login", login)
-	mux.HandleFunc("/logout", logout)
-	mux.HandleFunc("signup", signup)
-	mux.HandleFunc("/signup_account", signupAccount)
+	//mux.HandleFunc("/err", err)
+	//mux.HandleFunc("/login", login)
+	//mux.HandleFunc("/logout", logout)
+	//mux.HandleFunc("signup", signup)
+	//mux.HandleFunc("/signup_account", signupAccount)
 	mux.HandleFunc("/authenticate", authenticate)
-	mux.HandleFunc("/thread/new", newThread)
-	mux.HandleFunc("/thread/create", createThread)
-	mux.HandleFunc("/thread/post", postThread)
-	mux.HandleFunc("/thread/read", readThread)
+	//mux.HandleFunc("/thread/new", newThread)
+	//mux.HandleFunc("/thread/create", createThread)
+	//mux.HandleFunc("/thread/post", postThread)
+	//mux.HandleFunc("/thread/read", readThread)
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
 		Handler: mux,

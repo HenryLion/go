f418bd123daa4d7896d652d5860dea6cd533b255
anbreakdy <anbreakdy@outlook.com>
2023-11-30 23:38:16 +0800

    learn send and get cookie

---
+++ go_web_programming/cookie/send_cookie_to_browser.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	c1 := http.Cookie{
+		Name:     "first_cookie",
+		Value:    "Go Web Programming",
+		HttpOnly: true,
+	}
+	c2 := http.Cookie{
+		Name:     "second_cookie",
+		Value:    "Manning Publications Co",
+		HttpOnly: true,
+	}
+	w.Header().Set("Set-Cookie", c1.String())
+	w.Header().Add("Set-Cookie", c2.String())
+	fmt.Fprintf(w, "hello,pencilhan!")
+}
+
+func getCookie(w http.ResponseWriter, r *http.Request) {
+	h := r.Header["Cookie"]
+	fmt.Fprintln(w, h)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/set_cookie", setCookie)
+
+	http.HandleFunc("/get_cookie", getCookie)
+	server.ListenAndServe()
+}

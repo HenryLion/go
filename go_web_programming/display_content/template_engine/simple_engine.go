11c80c01e6c24a17688b535c46d389549ca93fe7
anbreakdy <anbreakdy@outlook.com>
2023-12-01 23:56:23 +0800

    learn template engine

---
+++ go_web_programming/display_content/template_engine/simple_engine.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func process(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("tmpl.html")
+
+	// Ma Shang Hui Lai 字符串会替换tmpl.html中的占位符
+	t.Execute(w, "Ma Shang Hui Lai")
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/process", process)
+	server.ListenAndServe()
+}

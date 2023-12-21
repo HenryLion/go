a74d96aba727646eb4dbee78ba322752cbbef5f1
anbreakdy <anbreakdy@outlook.com>
2023-12-21 23:37:07 +0800

    learn anonymous func

---
+++ go101/go101_basic/anonymous_func.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+	x, y := func() (int, int) {
+		println("This function has no parameters.")
+		return 3, 4
+	}()
+	println(x, y)
+
+	func(a, b int) {
+		println("a*a + b*b = ", a*a+b*b)
+	}(x, y)
+
+	func(x int) {
+		println("x*x + y*y =", x*x+y*y)
+	}(y)
+
+	func() {
+		println("x*x + y*y = ", x*x+y*y)
+	}()
+}

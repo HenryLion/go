47a513ae7401321002b82f43d5ec0771786d3e41
anbreakdy <anbreakdy@outlook.com>
2023-12-23 16:06:18 +0800

    learn import _ package

---
+++ go101/go101_basic/package_and_import/init_func.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+//import (
+//	"database/sql"
+//	_ "github.com/go-sql-driver/mysql" 导入此匿名包是为了初始化mysql驱动，以便database/sql包可以调用sql.Open连接数据库
+//)
+
+func init() {
+	fmt.Println("hi,", bob)
+}
+
+func main() {
+	fmt.Println("bye")
+}
+
+func init() {
+	fmt.Println("hello,", smith)
+}
+
+func titledName(who string) string {
+	return "Mr. " + who
+}
+
+var bob, smith = titledName("Bob"), titledName("Smith")

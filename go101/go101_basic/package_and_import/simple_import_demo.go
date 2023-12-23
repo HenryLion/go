bef8b46d8aa7a77bf1442bdd9bf7d700be65b39c
anbreakdy <anbreakdy@outlook.com>
2023-12-23 15:32:39 +0800

    learn import

--- go101/go101_basic/package_and_import/simple_import_demo.go
+++ go101/go101_basic/package_and_import/simple_import_demo.go
@@ -7,5 +7,6 @@ import (
 
 func main() {
 	fmt.Println("Go has", 25, "keywords.")
-	fmt.Printf("Next pseudo-random number is %v.\n", rand.Uint32())
+	var num = rand.Uint32()
+	fmt.Printf("Next pseudo-random number is %v, %x, %x.\n", num, num, "hello")
 }

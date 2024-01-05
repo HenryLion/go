4cbc69b2c98793ab46d88bbf82b2ba0901fc617c
anbreakdy <anbreakdy@outlook.com>
2024-01-06 00:49:18 +0800

    learn method

--- go101/go101_basic/go_type_system/methods/method.go
+++ go101/go101_basic/go_type_system/methods/method.go
@@ -12,7 +12,16 @@ func FilterEven(in int) bool {
 	return in%2 == 0
 }
 
+type Age int
+
+func (a Age) PrintSome() int {
+	fmt.Println("hello,world")
+	return 0
+}
+
 func main() {
 	var filter FilterFunc = FilterEven
 	fmt.Println(filter.Filte(3))
+	var a Age
+	_ = a.PrintSome()
 }
